fd13b26cbb56da9b83adc7bbfb557a17fece6a34
EbbaSchmid <justebba@gmail.com>
2022-09-20 18:53:28 -0500

    routes/skills.js update

--- routes/skills.js
+++ routes/skills.js
@@ -1,14 +1,23 @@
-app.get('/', function(req, res) {
-    res.render('skills/index', {
-        skills: skills
-    })
-})
+// app.get('/', function(req, res) {
+//     res.render('skills/index', {
+//         skills: skills
+//     })
+// })
 
 import { Router } from 'express'
-// import the Skills data
-
-import * as skillsCtrl from '../controllers/skills.js'
-
-router.get('/', skillsCtrl.index)
 
 const router = Router()
+
+router.get('/', function(req, res) {
+    res.send('respond with a resource')
+})
+
+
+router.get('/done', function (req, res){
+    res.send('respond with done')
+})
+
+export {
+    router
+}
+
