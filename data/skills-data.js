2a5a385bedc5c85147c87a0040e022a1e68d2b1c
EbbaSchmid <justebba@gmail.com>
2022-09-20 18:13:28 -0500

    data update

---
+++ data/skills-data.js
@@ -0,0 +1,9 @@
+const skills = [
+    {activity: 'badminton', cool: true, weather: 'no wind'},
+    {activity: 'golf', cool: false, weather: 'no lightening'},
+    {activity: 'basketball', cool: true, weather: 'no rain'},
+]
+
+export { 
+    skills
+}
