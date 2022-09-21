b766d488df01cf1cb6299dbd6f64349e8d8d77e5
EbbaSchmid <justebba@gmail.com>
2022-09-20 19:49:31 -0500

    update

--- controllers/skills.js
+++ controllers/skills.js
@@ -1,10 +1,18 @@
-import { skills } from '../data/skill-data.js'
+import { Skill } from '../models/skill.js'
 
 function index(req, res) {
-    res.render('skills/index', {
-        skills: skills
+    Todo.find({})
+    .then(todos => { // todos represents the result of the query, in this case ALL todos
+        res.render('todos/index', {
+        todos: todos,
+    })
+    })
+    .catch(error => { // If there's an error, console.log it and redirect back home!
+        console.log(error)
+        res.redirect('/')
     })
 }
+
 
     export {
         index
