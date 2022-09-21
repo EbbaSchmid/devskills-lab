e228e9358bf0c67d4a0993b557d143a39aedf54f
EbbaSchmid <justebba@gmail.com>
2022-09-21 16:14:34 -0500

    create export add

--- controllers/skills.js
+++ controllers/skills.js
@@ -3,12 +3,12 @@ import { Skill } from '../models/skill.js'
 
 function index(req, res) {
     Skill.find({})
-    .then(skills => { // todos represents the result of the query, in this case ALL todos
+    .then(skills => { 
         res.render('skills/index', {
         skills: skills,
     })
     })
-    .catch(error => { // If there's an error, console.log it and redirect back home!
+    .catch(error => { 
         console.log(error)
         res.redirect('/')
     })
@@ -22,12 +22,11 @@ function create(req, res) {
     console.log(req.body)
     Skill.create(req.body)
     .then(skill => {
-          // Notice we are doing a redirect here!
         res.redirect('/skills')
     })
     .catch(error => {
         console.log(error)
-        res.redirect('/skills')
+        res.redirect('/')
     })
 }
 
@@ -35,4 +34,5 @@ function create(req, res) {
 export {
     index,
     newSkill as new,
+    create
 }
