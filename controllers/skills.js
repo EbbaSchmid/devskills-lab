da6ca30f61472023d961e037cb3a9ff120779feb
EbbaSchmid <justebba@gmail.com>
2022-09-21 15:56:54 -0500

    controller routes

--- controllers/skills.js
+++ controllers/skills.js
@@ -1,4 +1,5 @@
 import { Skill } from '../models/skill.js'
+import *
 
 function index(req, res) {
     Skill.find({})
--- routes/skills.js
+++ routes/skills.js
@@ -5,6 +5,7 @@
 // })
 
 import { Router } from 'express'
+import * as skillsCtrl from '../controllers/skills.js'
 
 const router = Router()
 
